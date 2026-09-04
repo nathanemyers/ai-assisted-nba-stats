5ba6a8a8657636c0102cccb46a1149b3020991f0
Nathan Myers <nathanemyers@gmail.com>
2026-09-03 19:22:45 -0500

    reorganzie code

--- src/client/config.ts
+++ src/client/config.ts
@@ -1,10 +1,15 @@
+enum Model {
+    // can be quite sluggish
+    GEMMA4_26B = 'gemma4:26b',
+}
+
 interface Config {
     ollamaHost: string,
-    model: string,
+    model: Model,
 }
 const config: Config = {
     ollamaHost: "http://127.0.0.1:11434",
-    model: 'gemma4:26b',
+    model: Model.GEMMA4_26B,
 }
 
 export default config
