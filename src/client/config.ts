3e9d64283ee7a868dd706fb8c38be61992f995e8
nathan myers <nathanemyers@gmail.com>
2026-09-17 13:00:03 -0500

    add lightweight models

--- src/client/config.ts
+++ src/client/config.ts
@@ -1,6 +1,9 @@
 enum Model {
   // can be quite sluggish
   GEMMA4_26B = 'gemma4:26b',
+  QWEN3_27B_K2 = 'hf.co/unsloth/Qwen3.8-27B-GGUF:UD-IQ2_XXS',
+  // actually runs on my graphics card
+  QWEN3_4B_K_M = 'hf.co/unsloth/Qwen3-4B-GGUF:Q4_K_M'
 }
 
 interface Config {
@@ -9,7 +12,7 @@ interface Config {
 }
 const config: Config = {
   ollamaHost: 'http://127.0.0.1:11434',
-  model: Model.GEMMA4_26B,
+  model: Model.QWEN3_4B_K_M,
 }
 
 export default config
